5e7763542435aeaa80c2d7f89a9cd60aeda50d58
SirPotatoIV <otoolej33@gmail.com>
2019-11-18 17:37:54 -0600

    Forgot to save script.js before previous 2 commits. This commit contains the addition of the psuedo code. Added styling to start button.

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 function quizGame(){
     console.log("quiz game test");
+    // Define variables to store questions, time, current score, all-time score
+    
+    // Create button to start game
+    // Start a timer and display countdown
+    // Create function to generate html for questions
+    // Get user answer
+    // Check if user answer is correct
+    // Decide if time should be subtracted
+    // Update variable to store if answer was right or wrong
+    // Calculate final score
+    // Display Score
+    // Collect User Initials and store score
+    // Make code restart
 }
 quizGame()
