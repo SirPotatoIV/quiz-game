522817fa92fd9fccdf40dc0d7328309d0874f713
SirPotatoIV <otoolej33@gmail.com>
2019-11-18 18:03:08 -0600

    Created array of objects to store questions in questions.js. Copied structure from homework README. Tested using console.log.

--- questions.js
+++ questions.js
@@ -1,5 +1,20 @@
 function createQuestions() {
     console.log("questions.js linked.");
-
+    // Basis of code taken from homework README.
+    const questions = [
+          {
+            title: "Commonly used data types DO NOT include:",
+            choices: ["strings", "booleans", "alerts", "numbers"],
+            answer: "alerts"
+          },
+          {
+            title: "The condition in an if / else statement is enclosed within ____.",
+            choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+            answer: "parentheses"
+          },
+          
+        ];
+    
+    console.log(questions);
 }
 createQuestions();
