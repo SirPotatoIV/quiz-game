1fee48f482a4bb4b2aa5c558a1a7954cf01f16f7
SirPotatoIV <otoolej33@gmail.com>
2019-11-18 18:30:58 -0600

    Started adding variables that will be needed for the script.js to work. Started building function to keep track of time. Currently the function starts when page is loaded and counts down for one minute. Interval currently continues even after time runs out.

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
 function quizGame(){
     console.log("script.js linked.");
-    // Define variables to store questions, time, current score, all-time score
-    
+    // Define variables to store questions, time, score, all-time score
+    // -- See questions.js for variable being used to store the questions
+    let time = 0;
+    const defaultTime = 60;
+    const score = 0;
+    const highScores = [];
+
     // Create button to start game
+    // -- This was done with HTML. No need to dynamically generate ... currently
+
     // Start a timer and display countdown
+    function timer() {
+        time = defaultTime;
+        mainInterval = setInterval(function(){
+            time = time - 1;
+
+            console.log("timer test:"+ time);
+        }, 1000);
+
+    }
+    timer();
     // Create function to generate html for questions
     // Get user answer
     // Check if user answer is correct
